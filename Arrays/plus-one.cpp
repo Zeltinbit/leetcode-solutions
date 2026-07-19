2b74f12b74ef72c6f34df8a87a3bf73da6d69147
Zeltinbit <Tinbit23@gmail.com>
2026-07-19 23:25:00 +0300

    Solve LeetCode #66 Plus One

---
+++ Arrays/plus-one.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+
+        for (int i = digits.size() - 1; i >= 0; i--)
+        {
+
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        digits.insert(digits.begin(), 1);
+
+        return digits;
+    }
+};
+
+int main()
+{
+
+    Solution solution;
+
+    vector<int> digits = {9, 9, 9};
+
+    vector<int> result = solution.plusOne(digits);
+
+    cout << "Result: ";
+
+    for (int digit : result)
+    {
+        cout << digit << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
