5aff28369c510b624b16b22ca9c739e3ac4f184d
Zeltinbit <Tinbit23@gmail.com>
2026-07-24 21:58:25 +0300

    Solved LeetCode 53 - Maximum Subarray using Kadane's Algorithm

---
+++ Arrays/Maximum-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int sum = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            sum = max(nums[i], sum + nums[i]);
+            if (sum > maxSum)
+            {
+                maxSum = sum;
+            }
+        }
+        return maxSum;
+    }
+};
